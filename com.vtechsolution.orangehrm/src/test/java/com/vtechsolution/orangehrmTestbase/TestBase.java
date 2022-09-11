ddba73b19091e0c4a0d9f7e45a5465a9a6f9713c
user <user@Dell-pc>
2022-09-11 12:24:33 +0530

    Implements Extends Reports in Base class

--- com.vtechsolution.orangehrm/src/test/java/com/vtechsolution/orangehrmTestbase/TestBase.java
+++ com.vtechsolution.orangehrm/src/test/java/com/vtechsolution/orangehrmTestbase/TestBase.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 //import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -17,6 +18,10 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import com.vtechsolution.orangehrmutility.ConfigDataProvider;
 import com.vtechsolution.orangehrmutility.ExcelDataProvider;
 import com.vtechsolution.orangehrmutility.constantvariable;
@@ -29,11 +34,29 @@ public class TestBase {
 	public WebDriver driver;
 	public static ConfigDataProvider ConfigDataProvider;
 	public static ExcelDataProvider excelDataProvider;
+	public static ExtentHtmlReporter extentHtmlReporter;
+	public static ExtentReports extentReports;
+	public static ExtentTest extentTest;
 	    @BeforeSuite
 	   public void init()
 	   {
 	    	ConfigDataProvider = new ConfigDataProvider(constantvariable.ConfigDataPath);
 	    	excelDataProvider  = new ExcelDataProvider(constantvariable.excelPath);
+	    	
+	    	extentHtmlReporter = new ExtentHtmlReporter("./Reports/extentReports.html");
+	    	extentHtmlReporter.config().setDocumentTitle("Automation Test Reports");
+	    	extentHtmlReporter.config().setReportName("RT Test Reports");
+	    	extentHtmlReporter.config().setTheme(Theme.STANDARD);
+	    	
+	    	extentReports = new ExtentReports();
+	    	extentReports.attachReporter(extentHtmlReporter);
+	    	
+	    	extentReports.setSystemInfo("Hostname","Local Host");
+	    	extentReports.setSystemInfo("OS","Window");
+	    	extentReports.setSystemInfo("Browser","Chrome");
+	    	extentReports.setSystemInfo("TE","kishor");
+	    	extentReports.setSystemInfo("Test Cases","RT");
+	    	
 	   }
 
 	@BeforeMethod
@@ -66,4 +89,9 @@ public class TestBase {
 		driver.quit();
 	}
 
+	@AfterTest
+     public void extentFlush()
+     {
+		extentReports.flush();
+     }
 }
--- com.vtechsolution.orangehrm/src/test/java/com/vtechsolution/orangehrmTestcases/Login_Tc01.java
+++ com.vtechsolution.orangehrm/src/test/java/com/vtechsolution/orangehrmTestcases/Login_Tc01.java
@@ -4,16 +4,19 @@ import java.util.concurrent.TimeUnit;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentTest;
 import com.vtechsolution.orangehrmTestbase.TestBase;
 import com.vtechsolution.orangehrmpageobject.LoginPage;
 
 public class Login_Tc01 extends TestBase {
 	LoginPage lp;
 
-	@Test(priority = 3)
+	@Test(priority = 4)
 	public void LoginTc_o1() {
 		
-
+		extentTest = extentReports.createTest("LoginTc_o1");
+		
 		LoginPage lp = new LoginPage(driver);
 		lp.setUsername(ConfigDataProvider.getUserName());
 		lp.setPassword(ConfigDataProvider.getPassword());
@@ -21,26 +24,31 @@ public class Login_Tc01 extends TestBase {
 
 		if (driver.getPageSource().contains("PIM")) {
 			Assert.assertTrue(true);
-			System.out.println("LoginSuccess...");
+			extentTest.info("LoginSuccess...");
+			//System.out.println("LoginSuccess...");
 		} else {
 			Assert.assertTrue(false);
+			extentTest.info("Login fails..");
 			System.out.println("Login fails..");
 		}
 	}
 
-	@Test(priority = 2)
+	@Test(priority = 2,enabled = false)
 	public void verifyOrangeHrmlogo() {
 
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		extentTest = extentReports.createTest("verifyOrangeHrmlogo");
 		
 		lp = new LoginPage(driver);
 		if (lp.verifyOrangeHrmlogo()) {
 			Assert.assertTrue(true);
-			System.out.println("Orange HRM Logo is Displayed on Login Page");
+			extentTest.info("Orange HRM Logo is Displayed on Login Page");
+			//System.out.println("Orange HRM Logo is Displayed on Login Page");
 		} 
 		else {
 			
 			System.out.println("Orange HRM Logo is not Displayed on Login Page");
+			extentTest.info("Orange HRM Logo is not Displayed on Login Page");
 			Assert.assertTrue(false);
 
 		}
@@ -49,13 +57,17 @@ public class Login_Tc01 extends TestBase {
 	@Test(priority = 1)
 	public void verifyLoginPageTitleTc() {
 		
-
+		 extentTest = extentReports.createTest("verifyLoginPageTitleTc");
+		
 		if (driver.getTitle().equals("OrangeHRM")) {
 			Assert.assertTrue(true);
-			System.out.println("Login Page title is verifyed");
+			extentTest.info("Login Page title is verifyed");
+			//System.out.println("Login Page title is verifyed");
 		} else {
 			Assert.assertTrue(false);
-			System.out.println("Login Page title  is not matched with expected title");
+			extentTest.info("Login Page title  is not matched with expected title");
+			
+			//System.out.println("Login Page title  is not matched with expected title");
 
 		}
 	}
